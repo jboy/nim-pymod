968e1ffe81e7b01ffde370bf1b8a2fddef60ee5d
James Boyden <james@snapdisco.com>
2015-11-28 15:57:08 +1100

    Run the Pytest tests with all available versions of Python.
    Abort all further tests (including all remaining versions of Python) if any test fails.

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -2,8 +2,7 @@
 
 PYTEST_COMPAT="000-return_string_HelloWorld"
 PYTEST_MODULE="pytest"
-#for pyver in python python2 python3
-for pyver in python3
+for pyver in python python2 python3
 do
 	if "$pyver" --version >/dev/null 2>/dev/null
 	then
@@ -12,9 +11,16 @@ do
 		if ! "$pyver" -c "import $PYTEST_MODULE" >/dev/null 2>/dev/null
 		then
 			echo "Can't find Pytest testing framework: Python import '$PYTEST_MODULE' failed" >&2
-			echo 'Aborting tests.' >&2
+			echo 'Aborting all tests.' >&2
 			exit 1
 		fi
-		"$pyver" -m "$PYTEST_MODULE" "$@" "$PYTEST_COMPAT"
+		if ! "$pyver" -m "$PYTEST_MODULE" "$@" "$PYTEST_COMPAT"
+		then
+			echo
+			echo "Test failure with '$pyver' executable" >&2
+			echo 'Aborting all tests.' >&2
+			exit 1
+		fi
 	fi
+	echo
 done
