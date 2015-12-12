0e82ec3dedc3871d139933cda3e156b2e8f72eea
James Boyden <james@snapdisco.com>
2015-12-13 03:34:31 +1100

    Test the `arr.data -> pointer` attribute.
    It took me a long time to find out how to access the actual `arr.data` pointer!  If you just invoke `arr.data` in Python, it returns you a temporary intermediate buffer object, with a different memory address!

--- tests/013-numpy_array_attrs/test_numpy_array_attrs.nim
+++ tests/013-numpy_array_attrs/test_numpy_array_attrs.nim
@@ -1,11 +1,29 @@
 import strutils  # `%`
 import pymod
+import pymodpkg/miscutils
 import pymodpkg/pyarrayobject
 
 
 proc returnDtypeAsString*(arr: ptr PyArrayObject): string {.exportpy.} = $arr.dtype
 
 proc returnDataPointerAsInt*(arr: ptr PyArrayObject): int {.exportpy.} = cast[int](arr.data)
+
+
+proc returnBoolDataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
+  let dt = arr.dtype
+  if dt != np_bool:
+    let msg = "expected input array of dtype $1, received dtype $2" % [$np_int8, $dt]
+    raise newException(ValueError, msg)
+  else:
+    result = cast[int](arr.data(bool))
+
+proc returnInt8DataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
+  let dt = arr.dtype
+  if dt != np_int8:
+    let msg = "expected input array of dtype $1, received dtype $2" % [$np_int8, $dt]
+    raise newException(ValueError, msg)
+  else:
+    result = cast[int](arr.data(int8))
 
 proc returnInt16DataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
   let dt = arr.dtype
@@ -15,6 +33,41 @@ proc returnInt16DataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
   else:
     result = cast[int](arr.data(int16))
 
+proc returnInt32DataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
+  let dt = arr.dtype
+  if dt != np_int32:
+    let msg = "expected input array of dtype $1, received dtype $2" % [$np_int32, $dt]
+    raise newException(ValueError, msg)
+  else:
+    result = cast[int](arr.data(int32))
+
+proc returnInt64DataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
+  let dt = arr.dtype
+  if dt != np_int64:
+    let msg = "expected input array of dtype $1, received dtype $2" % [$np_int64, $dt]
+    raise newException(ValueError, msg)
+  else:
+    result = cast[int](arr.data(int64))
+
+proc returnFloat32DataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
+  let dt = arr.dtype
+  if dt != np_float32:
+    let msg = "expected input array of dtype $1, received dtype $2" % [$np_int32, $dt]
+    raise newException(ValueError, msg)
+  else:
+    result = cast[int](arr.data(float32))
+
+proc returnFloat64DataPtrAsInt*(arr: ptr PyArrayObject): int {.exportpy.} =
+  let dt = arr.dtype
+  if dt != np_float64:
+    let msg = "expected input array of dtype $1, received dtype $2" % [$np_int64, $dt]
+    raise newException(ValueError, msg)
+  else:
+    result = cast[int](arr.data(float64))
+
 
 initPyModule("",
-    returnDtypeAsString, returnDataPointerAsInt, returnInt16DataPtrAsInt)
+    returnDtypeAsString, returnDataPointerAsInt,
+    returnBoolDataPtrAsInt, returnInt8DataPtrAsInt, returnInt16DataPtrAsInt,
+    returnInt32DataPtrAsInt, returnInt64DataPtrAsInt,
+    returnFloat32DataPtrAsInt, returnFloat64DataPtrAsInt)
--- tests/013-numpy_array_attrs/test_numpy_array_attrs.py
+++ tests/013-numpy_array_attrs/test_numpy_array_attrs.py
@@ -1,9 +1,22 @@
 import numpy
 import pytest
+import time
 
 
 def test_0_compile_pymod_test_mod(pmgen_py_compile):
         pmgen_py_compile(__name__)
+
+
+@pytest.fixture(scope="module")
+def seeded_random_number_generator():
+    """Ensure that the random number generator has been seeded."""
+    numpy.random.seed(int(time.time()))
+
+
+@pytest.fixture
+def random_1d_array_size(request, seeded_random_number_generator):
+    """Return a random integer to be the size of an array."""
+    return numpy.random.randint(1, 50)
 
 
 @pytest.mark.parametrize("input_type,input_type_str", [
@@ -19,8 +32,29 @@ def test_0_compile_pymod_test_mod(pmgen_py_compile):
         (numpy.float32, "numpy.float32"),
         (numpy.float64, "numpy.float64"),
 ])
-def test_returnDtypeAsString(pymod_test_mod, input_type, input_type_str):
-    arg = numpy.zeros(5, dtype=input_type)
+def test_returnDtypeAsString(pymod_test_mod, random_1d_array_size, input_type, input_type_str):
+    arg = numpy.zeros(random_1d_array_size, dtype=input_type)
     res = pymod_test_mod.returnDtypeAsString(arg)
     assert res == input_type_str
+
+
+@pytest.mark.parametrize("input_type", [
+        numpy.bool,
+        numpy.int8,
+        numpy.int16,
+        numpy.int32,
+        numpy.int64,
+        numpy.uint8,
+        numpy.uint16,
+        numpy.uint32,
+        numpy.uint64,
+        numpy.float32,
+        numpy.float64,
+])
+def test_returnDataPointerAsInt(pymod_test_mod, random_1d_array_size, input_type):
+    arg = numpy.zeros(random_1d_array_size, dtype=input_type)
+    res = pymod_test_mod.returnDataPointerAsInt(arg)
+
+    data_addr = arg.__array_interface__["data"][0]
+    assert res == data_addr
 
