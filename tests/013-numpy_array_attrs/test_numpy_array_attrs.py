c35b05fb0c28fabf2d7e961ccb87b30e6174e645
James Boyden <james@snapdisco.com>
2015-12-13 15:54:17 +1100

    Add tests for the pointer value of `.data(T)` for several Nim/Numpy types.

--- tests/013-numpy_array_attrs/test_numpy_array_attrs.py
+++ tests/013-numpy_array_attrs/test_numpy_array_attrs.py
@@ -39,6 +39,10 @@ def test_returnPyArrayObjectPtrAsInt(pymod_test_mod, random_1d_array_size, input
 
 
 @pytest.mark.parametrize("input_type,input_type_str", [
+        # NOTE:  Pymod returns the type string "<type 'numpy.bool'>" rather than "<type 'bool'>",
+        # which is what `str(numpy.bool)` returns in Python.
+        # Pymod returns "<type 'numpy.bool'>" for consistency with all other Numpy type strings,
+        # which are all of the form "<type 'numpy.xxxx'>" (eg, "<type 'numpy.int8'>").
         (numpy.bool,    "numpy.bool"),
         (numpy.int8,    "numpy.int8"),
         (numpy.int16,   "numpy.int16"),
@@ -57,6 +61,15 @@ def test_returnDtypeAsString(pymod_test_mod, random_1d_array_size, input_type, i
     assert res == input_type_str
 
 
+def _get_array_data_address(arr):
+    # It took me a long time to find out how to access the `arr.data` address
+    # (ie, obtain the actual `arr.data` pointer as an integer) in Python!
+    # If you simply invoke `arr.data` in Python, it returns you a temporary
+    # intermediate buffer object, that has a different memory address!
+    data_addr = arr.__array_interface__["data"][0]
+    return data_addr
+
+
 @pytest.mark.parametrize("input_type", [
         numpy.bool,
         numpy.int8,
@@ -73,10 +86,49 @@ def test_returnDtypeAsString(pymod_test_mod, random_1d_array_size, input_type, i
 def test_returnDataPointerAsInt(pymod_test_mod, random_1d_array_size, input_type):
     arg = numpy.zeros(random_1d_array_size, dtype=input_type)
     res = pymod_test_mod.returnDataPointerAsInt(arg)
+    data_addr = _get_array_data_address(arg)
+    assert res == data_addr
 
-    # It took me a long time to find out how to access the actual `arg.data`
-    # pointer!  If you just invoke `arg.data` in Python, it returns you a
-    # temporary intermediate buffer object, with a different memory address!
-    data_addr = arg.__array_interface__["data"][0]
+
+def test_returnBoolDataPtrAsInt(pymod_test_mod, random_1d_array_size):
+    arg = numpy.zeros(random_1d_array_size, dtype=numpy.bool)
+    res = pymod_test_mod.returnBoolDataPtrAsInt(arg)
+    data_addr = _get_array_data_address(arg)
+    assert res == data_addr
+
+def test_returnInt8DataPtrAsInt(pymod_test_mod, random_1d_array_size):
+    arg = numpy.zeros(random_1d_array_size, dtype=numpy.int8)
+    res = pymod_test_mod.returnInt8DataPtrAsInt(arg)
+    data_addr = _get_array_data_address(arg)
+    assert res == data_addr
+
+def test_returnInt16DataPtrAsInt(pymod_test_mod, random_1d_array_size):
+    arg = numpy.zeros(random_1d_array_size, dtype=numpy.int16)
+    res = pymod_test_mod.returnInt16DataPtrAsInt(arg)
+    data_addr = _get_array_data_address(arg)
+    assert res == data_addr
+
+def test_returnInt32DataPtrAsInt(pymod_test_mod, random_1d_array_size):
+    arg = numpy.zeros(random_1d_array_size, dtype=numpy.int32)
+    res = pymod_test_mod.returnInt32DataPtrAsInt(arg)
+    data_addr = _get_array_data_address(arg)
+    assert res == data_addr
+
+def test_returnInt64DataPtrAsInt(pymod_test_mod, random_1d_array_size):
+    arg = numpy.zeros(random_1d_array_size, dtype=numpy.int64)
+    res = pymod_test_mod.returnInt64DataPtrAsInt(arg)
+    data_addr = _get_array_data_address(arg)
+    assert res == data_addr
+
+def test_returnFloat32DataPtrAsFloat(pymod_test_mod, random_1d_array_size):
+    arg = numpy.zeros(random_1d_array_size, dtype=numpy.float32)
+    res = pymod_test_mod.returnFloat32DataPtrAsInt(arg)
+    data_addr = _get_array_data_address(arg)
+    assert res == data_addr
+
+def test_returnFloat64DataPtrAsFloat(pymod_test_mod, random_1d_array_size):
+    arg = numpy.zeros(random_1d_array_size, dtype=numpy.float64)
+    res = pymod_test_mod.returnFloat64DataPtrAsInt(arg)
+    data_addr = _get_array_data_address(arg)
     assert res == data_addr
 
