aa7922c2b26c99f5ae6c1066744c5abb94aff105
James Boyden <james@snapdisco.com>
2015-12-15 01:42:51 +1100

    Added tests for while loops & for loops of Forward Iterators.

--- tests/01-numpy_arrays/010-int32_find_max/test_int32_find_max.py
+++ tests/01-numpy_arrays/010-int32_find_max/test_int32_find_max.py
@@ -9,11 +9,41 @@ def test_0_compile_pymod_test_mod(pmgen_py_compile):
 
 ndims_to_test = [1, 2, 3, 4]
 
+# for loop, values
+
 @pytest.mark.parametrize("ndim", ndims_to_test)
 def test_int32FindMaxForLoopValues(pymod_test_mod, seeded_random_number_generator, ndim):
     arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
-    print ("arg = %s" % arg)
+    print ("\nrandom number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
+            (seeded_random_number_generator, ndim, arg.shape, arg))
     expectedRes = arg.max()
     res = pymod_test_mod.int32FindMaxForLoopValues(arg)
+    print ("res = %s" % str(res))
+    assert res == expectedRes
+
+
+# while loop, Forward Iter
+
+@pytest.mark.parametrize("ndim", ndims_to_test)
+def test_int32FindMaxWhileLoopForwardIter(pymod_test_mod, seeded_random_number_generator, ndim):
+    arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
+    print ("\nrandom number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
+            (seeded_random_number_generator, ndim, arg.shape, arg))
+    expectedRes = arg.max()
+    res = pymod_test_mod.int32FindMaxWhileLoopForwardIter(arg)
+    print ("res = %s" % str(res))
+    assert res == expectedRes
+
+
+# for loop, Forward Iter
+
+@pytest.mark.parametrize("ndim", ndims_to_test)
+def test_int32FindMaxForLoopForwardIter(pymod_test_mod, seeded_random_number_generator, ndim):
+    arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
+    print ("\nrandom number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
+            (seeded_random_number_generator, ndim, arg.shape, arg))
+    expectedRes = arg.max()
+    res = pymod_test_mod.int32FindMaxForLoopForwardIter(arg)
+    print ("res = %s" % str(res))
     assert res == expectedRes
 
