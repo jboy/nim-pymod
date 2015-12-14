7f946cb000f000f391bdea26f044cc27cff41810
James Boyden <james@snapdisco.com>
2015-12-15 03:34:16 +1100

    Test the Random Access Iter dereferencing alternatives where the Nim test function is parametrised by an integer `k`.

--- tests/01-numpy_arrays/010-int32_find_max/test_int32_find_max.py
+++ tests/01-numpy_arrays/010-int32_find_max/test_int32_find_max.py
@@ -79,3 +79,21 @@ def test_int32FindMaxWhileLoopRandaccIterDerefAlternatives(pymod_test_mod, seede
     print ("res = %s" % str(res))
     assert res == expectedRes
 
+
+@pytest.mark.parametrize("ndim", ndims_to_test)
+@pytest.mark.parametrize("nim_test_proc_name", [
+        "int32FindMaxWhileLoopRandaccIterIndexVsPlusOffsetK",
+        "int32FindMaxWhileLoopRandaccIterIndexVsMinusOffsetK",
+])
+@pytest.mark.parametrize("k", [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+def test_int32FindMaxWhileLoopRandaccIterDerefAlternatives(pymod_test_mod, seeded_random_number_generator,
+        ndim, nim_test_proc_name, k):
+    arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
+    print ("\nnim_test_proc_name = %s, k = %d" % (nim_test_proc_name, k))
+    print ("random number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
+            (seeded_random_number_generator, ndim, arg.shape, arg))
+    expectedRes = arg.max()
+    res = getattr(pymod_test_mod, nim_test_proc_name)(arg, k)
+    print ("res = %s" % str(res))
+    assert res == expectedRes
+
