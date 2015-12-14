722f63b9f3725714a29d4ff5d82ea84c47aa260d
James Boyden <james@snapdisco.com>
2015-12-15 03:23:21 +1100

    Test all equivalent alternatives for dereferencing a Random Access Iterator.

--- tests/01-numpy_arrays/010-int32_find_max/test_int32_find_max.py
+++ tests/01-numpy_arrays/010-int32_find_max/test_int32_find_max.py
@@ -52,42 +52,30 @@ def test_int32FindMaxForLoopForwardIter(pymod_test_mod, seeded_random_number_gen
 # while loop, Rand Acc Iter
 
 @pytest.mark.parametrize("ndim", ndims_to_test)
-def test_int32FindMaxWhileLoopRandaccIterDeref(pymod_test_mod, seeded_random_number_generator, ndim):
+@pytest.mark.parametrize("nim_test_proc_name", [
+        "int32FindMaxWhileLoopRandaccIterDeref",
+        "int32FindMaxWhileLoopRandaccIterIndex0",
+        "int32FindMaxWhileLoopRandaccIterDerefPlusZeroOffset",
+        "int32FindMaxWhileLoopRandaccIterDerefMinusZeroOffset",
+        "int32FindMaxWhileLoopRandaccIterIndexVsPlusOffset_1",
+        "int32FindMaxWhileLoopRandaccIterIndexVsPlusOffset_2",
+        "int32FindMaxWhileLoopRandaccIterIndexVsPlusOffset_3",
+        "int32FindMaxWhileLoopRandaccIterIndexVsPlusOffset_4",
+        "int32FindMaxWhileLoopRandaccIterIndexVsPlusOffset_5",
+        "int32FindMaxWhileLoopRandaccIterIndexVsMinusOffset_1",
+        "int32FindMaxWhileLoopRandaccIterIndexVsMinusOffset_2",
+        "int32FindMaxWhileLoopRandaccIterIndexVsMinusOffset_3",
+        "int32FindMaxWhileLoopRandaccIterIndexVsMinusOffset_4",
+        "int32FindMaxWhileLoopRandaccIterIndexVsMinusOffset_5",
+])
+def test_int32FindMaxWhileLoopRandaccIterDerefAlternatives(pymod_test_mod, seeded_random_number_generator,
+        ndim, nim_test_proc_name):
     arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
-    print ("\nrandom number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
+    print ("\nnim_test_proc_name = %s" % nim_test_proc_name)
+    print ("random number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
             (seeded_random_number_generator, ndim, arg.shape, arg))
     expectedRes = arg.max()
-    res = pymod_test_mod.int32FindMaxWhileLoopRandaccIterDeref(arg)
-    print ("res = %s" % str(res))
-    assert res == expectedRes
-
-@pytest.mark.parametrize("ndim", ndims_to_test)
-def test_int32FindMaxWhileLoopRandaccIterIndex0(pymod_test_mod, seeded_random_number_generator, ndim):
-    arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
-    print ("\nrandom number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
-            (seeded_random_number_generator, ndim, arg.shape, arg))
-    expectedRes = arg.max()
-    res = pymod_test_mod.int32FindMaxWhileLoopRandaccIterIndex0(arg)
-    print ("res = %s" % str(res))
-    assert res == expectedRes
-
-@pytest.mark.parametrize("ndim", ndims_to_test)
-def test_int32FindMaxWhileLoopRandaccIterDerefPlusZeroOffset(pymod_test_mod, seeded_random_number_generator, ndim):
-    arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
-    print ("\nrandom number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
-            (seeded_random_number_generator, ndim, arg.shape, arg))
-    expectedRes = arg.max()
-    res = pymod_test_mod.int32FindMaxWhileLoopRandaccIterDerefPlusZeroOffset(arg)
-    print ("res = %s" % str(res))
-    assert res == expectedRes
-
-@pytest.mark.parametrize("ndim", ndims_to_test)
-def test_int32FindMaxWhileLoopRandaccIterDerefMinusZeroOffset(pymod_test_mod, seeded_random_number_generator, ndim):
-    arg = array_utils.get_random_Nd_array_of_ndim_and_type(ndim, numpy.int32)
-    print ("\nrandom number seed = %d\nndim = %d, shape = %s\narg =\n%s" % \
-            (seeded_random_number_generator, ndim, arg.shape, arg))
-    expectedRes = arg.max()
-    res = pymod_test_mod.int32FindMaxWhileLoopRandaccIterDerefMinusZeroOffset(arg)
+    res = getattr(pymod_test_mod, nim_test_proc_name)(arg)
     print ("res = %s" % str(res))
     assert res == expectedRes
 
