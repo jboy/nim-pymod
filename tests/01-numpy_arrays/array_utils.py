b31f0d44edda3287a7b1870933bf12b5f268e78d
James Boyden <james@snapdisco.com>
2015-12-15 13:29:34 +1100

    Deduplicated some duplicate array utility/fixture code.

--- tests/01-numpy_arrays/array_utils.py
+++ tests/01-numpy_arrays/array_utils.py
@@ -1,3 +1,18 @@
+# The utility functions in this module exist (ie, as non-fixtures) to enable us
+# to parametrize test functions (eg, by ndim, shape or numpy type) and then use
+# one of these utility functions within the test function to create the desired
+# array.
+#
+# (AFAICT, you can't supply the params of a parametrized test function into the
+# fixtures for that function.)
+#
+# These utility functions also enable us to specify just an ndim or shape value
+# as the fixture for a test function, then create the necessary array (using
+# that ndim or shape value) within the test function.  Often, the ndim or shape
+# is all we really care about in the test; not the specific randomly-generated
+# values in the array.
+
+
 import numpy
 
 all_supported_numpy_types = [
@@ -87,37 +102,48 @@ all_small_array_shapes = (all_0d_array_shapes + small_1d_array_shapes +
         indexable_small_array_shapes)
 
 
+def get_range_of_random_integers(target_type=None):
+    if target_type is not None:
+        if target_type in (numpy.bool, numpy.bool_):
+            # Since target type is `bool`, return [0, 1] as the range, so that
+            # there will be a more balanced distribution between True & False.
+            return (0, 1)
+
+    return (-100, 100)  # Small enough to fit into a positive int8.
+
+
+def get_random_1d_array_size():
+    # This is 2x the per-dimension size for 2+-D arrays.
+    return numpy.random.randint(20) + 1  # So the size is always >= 1.
+
+def get_random_Nd_array_dim():
+    return numpy.random.randint(10) + 1  # So the dimension is always >= 1.
+
+
 # 1-D arrays
 
-def get_random_1d_array_of_size_and_type(test_1d_array_size, test_type):
-    """Return a randomly-sized 1-D array of random integers in the range [-100, 100]."""
-    return numpy.random.random_integers(-100, 100, test_1d_array_size
-            ).astype(test_type)
+def get_random_1d_array_of_size_and_type(test_1d_array_size, test_type=None):
+    (lower, upper) = get_range_of_random_integers(test_type)
+    arr = numpy.random.random_integers(lower, upper, test_1d_array_size)
+    return arr if test_type is None else arr.astype(test_type)
 
 # N-D arrays
 
-def get_random_Nd_array_of_shape_and_type(test_Nd_array_shape, test_type):
-    """Return a randomly-shaped N-D array of random integers in the range [-100, 100]."""
-    array_size = numpy.prod(test_Nd_array_shape)
-    return numpy.random.random_integers(-100, 100, array_size).reshape(test_Nd_array_shape
-            ).astype(test_type)
-
-
 def get_random_Nd_array_shape(ndim):
     if ndim == 1:
-        dims = [numpy.random.randint(20) + 1]  # So the size is always >= 1.
+        return (get_random_1d_array_size(),)
     else:
-        dims = [
-                numpy.random.randint(10) + 1  # So the size is always >= 1.
-                for i in range(ndim)]
-
-    return tuple(dims)
+        return tuple([get_random_Nd_array_dim() for i in range(ndim)])
 
 
-def get_random_Nd_array_of_ndim_and_type(test_ndim, test_type):
-    """Return a randomly-shaped N-D array of random integers in the range [-100, 100]."""
-    array_shape = get_random_Nd_array_shape(test_ndim)
-    array_size = numpy.prod(array_shape)
-    return numpy.random.random_integers(-100, 100, array_size).reshape(array_shape
-            ).astype(test_type)
+def get_random_Nd_array_of_shape_and_type(test_Nd_array_shape, test_type=None):
+    array_size = numpy.prod(test_Nd_array_shape)
+    (lower, upper) = get_range_of_random_integers(test_type)
+    arr = numpy.random.random_integers(lower, upper, array_size).reshape(test_Nd_array_shape)
+    return arr if test_type is None else arr.astype(test_type)
+
+
+def get_random_Nd_array_of_ndim_and_type(test_ndim, test_type=None):
+    test_array_shape = get_random_Nd_array_shape(test_ndim)
+    return get_random_Nd_array_of_shape_and_type(test_array_shape, test_type)
 
--- tests/01-numpy_arrays/conftest.py
+++ tests/01-numpy_arrays/conftest.py
@@ -2,6 +2,12 @@ import numpy
 import pytest
 import time
 
+from array_utils import get_range_of_random_integers
+from array_utils import get_random_1d_array_size, get_random_Nd_array_dim
+from array_utils import get_random_1d_array_of_size_and_type
+from array_utils import get_random_Nd_array_shape
+from array_utils import get_random_Nd_array_of_shape_and_type
+
 
 @pytest.fixture(scope="module")
 def seeded_random_number_generator():
@@ -11,35 +17,22 @@ def seeded_random_number_generator():
     return seed
 
 
-_NUM_ELEMS_1D = 20  # 2x the per-dimension size for 2-D and larger.
-_NUM_ELEMS_ND = 10
-
-def _get_random_1d_array_size():
-    return numpy.random.randint(_NUM_ELEMS_1D) + 1  # So the size is always >= 1.
-
-def _get_random_Nd_array_dim():
-    return numpy.random.randint(_NUM_ELEMS_ND) + 1  # So the dimension is always >= 1.
-
-_RANDOM_INTEGER_RANGE = (-100, 100)  # Small enough to fit into a positive int8.
-
-
 # 1-D arrays
 
 @pytest.fixture
 def random_1d_array_size(seeded_random_number_generator):
     """Return a random integer in the range [1, 20] to be the size of a 1-D array."""
-    return _get_random_1d_array_size()
+    return get_random_1d_array_size()
 
 @pytest.fixture
 def random_1d_array_of_bool(random_1d_array_size):
     """Return a randomly-sized array of random bool values."""
-    return numpy.random.random_integers(0, 1, random_1d_array_size).astype(numpy.bool)
+    return get_random_1d_array_of_size_and_type(random_1d_array_size, numpy.bool)
 
 @pytest.fixture
 def random_1d_array_of_integers(random_1d_array_size):
     """Return a randomly-sized 1-D array of random integers in the range [-100, 100]."""
-    (lower, upper) = _RANDOM_INTEGER_RANGE
-    return numpy.random.random_integers(lower, upper, random_1d_array_size)
+    return get_random_1d_array_of_size_and_type(random_1d_array_size)
 
 
 # 2-D arrays
@@ -47,16 +40,12 @@ def random_1d_array_of_integers(random_1d_array_size):
 @pytest.fixture
 def random_2d_array_shape(seeded_random_number_generator):
     """Return a tuple of 2 random integers in the range [1, 10] to be the shape of a 2-D array."""
-    dim1 = _get_random_Nd_array_dim()
-    dim2 = _get_random_Nd_array_dim()
-    return (dim1, dim2)
+    return get_random_Nd_array_shape(2)
 
 @pytest.fixture
 def random_2d_array_of_integers(random_2d_array_shape):
     """Return a randomly-shaped 2-D array of random integers in the range [-100, 100]."""
-    array_size = numpy.prod(random_2d_array_shape)
-    (lower, upper) = _RANDOM_INTEGER_RANGE
-    return numpy.random.random_integers(lower, upper, array_size).reshape(random_2d_array_shape)
+    return get_random_Nd_array_of_shape_and_type(random_2d_array_shape)
 
 
 # 3-D arrays
@@ -64,10 +53,7 @@ def random_2d_array_of_integers(random_2d_array_shape):
 @pytest.fixture
 def random_3d_array_shape(seeded_random_number_generator):
     """Return a tuple of 3 random integers in the range [1, 10] to be the shape of a 3-D array."""
-    dim1 = _get_random_Nd_array_dim()
-    dim2 = _get_random_Nd_array_dim()
-    dim3 = _get_random_Nd_array_dim()
-    return (dim1, dim2, dim3)
+    return get_random_Nd_array_shape(3)
 
 
 # 4-D arrays
@@ -75,9 +61,5 @@ def random_3d_array_shape(seeded_random_number_generator):
 @pytest.fixture
 def random_4d_array_shape(seeded_random_number_generator):
     """Return a tuple of 4 random integers in the range [1, 10] to be the shape of a 4-D array."""
-    dim1 = _get_random_Nd_array_dim()
-    dim2 = _get_random_Nd_array_dim()
-    dim3 = _get_random_Nd_array_dim()
-    dim4 = _get_random_Nd_array_dim()
-    return (dim1, dim2, dim3, dim4)
+    return get_random_Nd_array_shape(4)
 
